6d03efb0d70aa2b4946a6b6fbd1a348e4840e59d
Ankur1326 <swamiankur46@gmail.com>
2024-01-07 14:45:02 +0530

    add getCurrentUser controller

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -260,6 +260,16 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, {}, "Password changed successfully!"));
 });
 
+const getCurrentUser = asyncHandler(async(req, res) => {
+  return res
+  .status(200)
+  .json(new ApiResponse(
+      200,
+      req.user,
+      "User fetched successfully"
+  ))
+})
+
 const updateAccountDetails = asyncHandler(async (req, res) => {
   const { fullName, email } = req.body;
 
@@ -475,8 +485,10 @@ export {
   logoutUser,
   refreshAccessToken,
   changeCurrentPassword,
+  getCurrentUser,
   updateAccountDetails,
   updateUserAvatar,
   updateUserCoverImage,
   getUserChannelProfile,
+  getWatchHistory
 };
