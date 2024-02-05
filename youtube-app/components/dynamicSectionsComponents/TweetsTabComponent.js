3e6224b271584dad13dd54b98995311f546e67d4
Ankur1326 <swamiankur46@gmail.com>
2024-02-05 20:56:17 +0530

    add TweetsTabComponent

---
+++ youtube-app/components/dynamicSectionsComponents/TweetsTabComponent.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const TweetsTabComponent = () => {
+  return (
+    <View>
+      <Text>TweetsTabComponent</Text>
+    </View>
+  )
+}
+
+export default TweetsTabComponent
+
+const styles = StyleSheet.create({})
