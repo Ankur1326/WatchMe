55ab49c5b9a7f9fb1e22ad1c203ba961599c6369
Ankur1326 <swamiankur46@gmail.com>
2024-02-05 20:56:58 +0530

    add VideoTabComponent

---
+++ youtube-app/components/dynamicSectionsComponents/VideoTabComponent.js
@@ -0,0 +1,57 @@
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
+import React, { useEffect } from 'react'
+
+const VideoTabComponent = () => {
+
+    useEffect(() => {
+        console.log("Video component is rendered");
+    }, [])
+
+    return (
+        <ScrollView>
+            <View style={{ backgroundColor: "yellow", }} >
+
+                <Text>VideoTabComponent</Text>
+                <Text>VideoTabComponent</Text>
+                <Text>VideoTabComponent</Text>
+                <Text>VideoTabComponent</Text>
+                <Text>VideoTabComponent</Text>
+                <Text>VideoTabComponent</Text>
+                <Text>VideoTabComponent</Text>
+                <Text>VideoTabComponent</Text>
+                <Text>VideoTabComponent</Text>
+                <Text>VideoTabComponent</Text>
+                <Text>VideoTabComponent</Text>
+                <Text>VideoTabComponent</Text>
+                <Text>VideoTabComponent</Text>
+                <Text>VideoTabComponent</Text>
+                <Text>VideoTabComponent</Text>
+                <Text>VideoTabComponent</Text>
+                <Text>VideoTabComponent</Text>
+                <Text>VideoTabComponent</Text>
+                <Text>VideoTabComponent</Text>
+                <Text>VideoTabComponent</Text>
+                <Text>VideoTabComponent</Text>
+                <Text>VideoTabComponent</Text>
+                <Text>VideoTabComponent</Text>
+                <Text>VideoTabComponent</Text>
+                <Text>VideoTabComponent</Text>
+                <Text>VideoTabComponent</Text>
+                <Text>VideoTabComponent</Text>
+                <Text>VideoTabComponent</Text>
+                <Text>VideoTabComponent</Text>
+                <Text>VideoTabComponent</Text>
+                <Text>VideoTabComponent</Text>
+                <Text>VideoTabComponent</Text>
+                <Text>VideoTabComponent</Text>
+                <Text>VideoTabComponent</Text>
+                <Text>VideoTabComponent</Text>
+                <Text>VideoTabComponent</Text>
+            </View>
+        </ScrollView>
+    )
+}
+
+export default VideoTabComponent
+
+const styles = StyleSheet.create({})
