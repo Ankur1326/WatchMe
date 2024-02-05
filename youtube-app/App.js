f215958ce1af808e457d63e18157687cdedf2a54
Ankur1326 <swamiankur46@gmail.com>
2024-02-05 20:53:14 +0530

    fixed navigation

--- youtube-app/App.js
+++ youtube-app/App.js
@@ -1,13 +1,19 @@
-import { StatusBar, StyleSheet, Text, View } from "react-native";
-import StackNavigation from "./navigations/StackNavigation";
+import 'react-native-gesture-handler';
+import { Image, Pressable, StatusBar, StyleSheet, Text, View } from "react-native";
+import StackNavigation, { MenuDrawer } from "./navigations/StackNavigation";
 import { UserContext } from "./UserContext";
+import { NavigationContainer } from "@react-navigation/native";
+import DrawerNavigation from './navigations/DrawerNavigation';
+import { Feather } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
 
 export default function App() {
+  // const navigation = useNavigation()
   return (
     <>
       <UserContext>
         <StatusBar barStyle="light-content" />
-        <StackNavigation />
+          <StackNavigation />
       </UserContext>
     </>
   );
