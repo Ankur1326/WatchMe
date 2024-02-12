853e662b9eb4053f718b596b88bf3b41be956684
Ankur1326 <swamiankur46@gmail.com>
2024-02-12 20:23:21 +0530

    add router

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,9 +18,21 @@ app.use(cookieParser());
 
 //routes import
 import userRouter from "./routes/user.routes.js";
+import videoRouter from "./routes/video.routes.js"
 
 //routes declaration for users
+
 app.use("/api/v1/users", userRouter);
+
+// app.use("/api/v1/healthcheck", healthcheckRouter)
+// app.use("/api/v1/tweets", tweetRouter)
+// app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("/api/v1/videos", videoRouter)
+// app.use("/api/v1/comments", commentRouter)
+// app.use("/api/v1/likes", likeRouter)
+// app.use("/api/v1/playlist", playlistRouter)
+// app.use("/api/v1/dashboard", dashboardRouter)
+
 
 // http://localhost:6100/api/v1/users/register
 
