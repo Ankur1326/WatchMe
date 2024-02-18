594e3d48b37e5f96c869b8783e76748f310da9e8
Ankur1326 <swamiankur46@gmail.com>
2024-02-18 08:17:34 +0530

    add function to deleteVideoFromCloudinary and extractPublicIdFromUrl

--- backend/src/utils/cloudinary.js
+++ backend/src/utils/cloudinary.js
@@ -24,7 +24,7 @@ const uploadOnCloudinary = async (localFilePath) => {
     });
     // file has been uploaded successfull
     //console.log("file is uploaded on cloudinary ", response.url);
-    // fs.unlinkSync(localFilePath);
+    fs.unlinkSync(localFilePath);
     return response;
   } catch (error) {
     console.error("Error uploading to Cloudinary:", error);
@@ -33,9 +33,61 @@ const uploadOnCloudinary = async (localFilePath) => {
     if (error && error.message && error.message.includes("Cloudinary")) {
       console.error("Cloudinary Error Details:", error);
     }
-    // fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got failed
+    fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got failed
     return null;
   }
 };
 
-export { uploadOnCloudinary };
+// function to delete image 
+const deleteImageFromCloudinary = async (url) => {
+  try {
+    const publicId = extractPublicIdFromUrl(url)
+
+    if (publicId) {
+      const deletedImage = await cloudinary.api.delete_resources([publicId])
+      // console.log("deletedImage : ", deletedImage);
+
+    }
+  } catch (error) {
+    console.log("error while deleting image", error);
+  }
+}
+
+// function to delete video 
+const deleteVideoFromCloudinary = async (videoUrl) => {
+  try {
+    const publicId = extractPublicIdFromUrl(videoUrl)
+    // console.log("video publicId : ", publicId);
+
+    const result = await cloudinary.uploader.destroy(publicId, {
+      resource_type: "video"
+    });
+
+    // console.log("result : ", result);
+
+    if (result.result == "ok") {
+      // console.log(`Video with public ID ${publicId} deleted successfully.`);
+    } else {
+      console.error(`Failed to delete video with public ID ${publicId}. Result:`, result);
+    }
+  } catch (error) {
+    console.error('Error deleting video:', error);
+  }
+
+}
+
+// extract publiId from url
+const extractPublicIdFromUrl = (url) => {
+  const parts = url.split('/')
+  const fileName = parts[parts.length - 1];
+
+  // https://res.cloudinary.com/dgq9fa4cp/image/upload/v1707584145/sdkpzqbhumtxcq5b7qdh.png
+  // this is publicId : sdkpzqbhumtxcq5b7qdh
+  const publicId = fileName.split('.')[0];
+
+  // console.log("publicId : ", publicId);
+  return publicId;
+
+}
+
+export { uploadOnCloudinary, deleteImageFromCloudinary, deleteVideoFromCloudinary };
