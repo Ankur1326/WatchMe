5b7d06776b88f6695b4ad3d79cebbf6e5afd637d
Ankur1326 <swamiankur46@gmail.com>
2024-02-28 12:02:07 +0530

    add header

--- youtube-app/screens/ChannelScreen.js
+++ youtube-app/screens/ChannelScreen.js
@@ -1,9 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import HeaderComponentt from '../components/HeaderComponent'
 
 const ChannelScreen = () => {
   return (
-    <View>
+    <View style={{backgroundColor: "#111"}} >
+      {/* header  */}
+      <HeaderComponentt />
       <Text>ChannelScreen</Text>
     </View>
   )
