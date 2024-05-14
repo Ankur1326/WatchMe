620e4bb9aab6605c1f0c309c89cbf0e491303e92
Ankur1326 <swamiankur46@gmail.com>
2024-05-14 19:41:33 +0530

    add toggleTweetLike controller

--- backend/src/controllers/like.controller.js
+++ backend/src/controllers/like.controller.js
@@ -199,18 +199,94 @@ const toggleCommentLike = asyncHandler(async (req, res) => {
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
     const { tweetId } = req.params
-
+    const { action } = req.body
     const userId = req.user._id
 
     if (!tweetId) {
         throw new ApiError(409, "tweetId is required")
     }
 
+    if (!action) {
+        throw new ApiError(409, "action is required")
+    }
+
     try {
+        // Check if user already liked this comment
+        const alreadyLiked = await Like.findOne({
+            tweet: tweetId,
+            likedBy: userId
+        });
+
+        // Check if user already disliked the video
+        const alreadyDisliked = await Dislike.findOne({
+            tweet: tweetId,
+            dislikedBy: userId
+        })
+
+        if (action === "like") {
+
+            if (alreadyDisliked) {
+                // await alreadyDisliked.remove(); // remove dislike
+                await Dislike.deleteOne({
+                    _id: alreadyDisliked._id
+                })
+            }
+
+            if (alreadyLiked) {
+                // await alreadyLiked.remove()
+                await Like.deleteOne({
+                    _id: alreadyLiked._id
+                })
+                return res.status(201).json(new ApiResponse(200, "tweet successfully unlike"));
+            } else {
+                // If not liked or disliked, create a new like
+                const newLike = await Like.create({
+                    tweet: tweetId,
+                    likedBy: userId
+                })
+
+                if (!newLike) {
+                    throw new ApiError(409, "tweet like is not created");
+                }
+                return res.status(201).json(new ApiResponse(200, newLike, "tweet like successfully created"));
+            }
+
+        } else if (action === "dislike") {
+
+            if (alreadyLiked) {
+                // await alreadyLiked.remove()
+                await Like.deleteOne({
+                    _id: alreadyLiked._id
+                })
+            }
+
+            if (alreadyDisliked) {
+                // await alreadyDisliked.remove(); // remove dislike
+                await Dislike.deleteOne({
+                    _id: alreadyDisliked._id
+                })
+                return res.status(201).json(new ApiResponse(200, "tweet dislike successfully removed"));
+            } else {
+                // If not liked or disliked, create a new dislike
+                const newDislike = await Dislike.create({
+                    tweet: tweetId,
+                    dislikedBy: userId
+                })
+
+                if (!newDislike) {
+                    throw new ApiError(409, "tweet dislike is not created");
+                }
+
+                return res.status(201).json(new ApiResponse(200, newDislike, "tweet dislike successfully created"));
+            }
+
+        } else {
+            throw new ApiError(409, "action must be in like or dislike")
+        }
 
     } catch (error) {
-        console.log("Internal server error while toggle tweet like: ", error);
-        throw new ApiError(500, "Internal server error while toggle tweet like: ", error)
+        console.log("Internal server error while toggle Tweet like or dislike: ", error);
+        throw new ApiError(500, "Internal server error while toggle Tweet like or dislike: ", error)
     }
 })
 
