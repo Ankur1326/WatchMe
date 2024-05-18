ea50738f9014d660ba62d8bce3bd280c3885639c
Ankur1326 <swamiankur46@gmail.com>
2024-05-18 20:11:40 +0530

    set routes for getChannelVideos

--- backend/src/routes/video.routes.js
+++ backend/src/routes/video.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { deleteVideo, getAllPublishVideo, getAllVideos, getVideoById, publishAVideo, togglePublishStatus, updateVideo } from "../controllers/video.controller.js";
+import { deleteVideo, getAllPublishVideo, getAllVideos, getChannelVideos, getVideoById, increaseVideoView, publishAVideo, togglePublishStatus, updateVideo } from "../controllers/video.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router()
 router.use(verifyJWT) // Apply verifyJWT middleware to all routes in this file 
 
+// to get all publish or unpublish videos
 router.route("/").get(getAllVideos).post(
     upload.fields([
         {
@@ -20,7 +21,11 @@ router.route("/").get(getAllVideos).post(
     publishAVideo
 )
 
+// to get all publish videos
 router.route("/getAll-publish-video").get(getAllPublishVideo)
+
+// get another user channle videos
+router.route("/getAll-anoter-channel-videos/:userId").get(getChannelVideos)
 
 router
     .route("/:videoId")
@@ -29,5 +34,7 @@ router
     .patch(upload.single("thumbnail"), updateVideo);
 
 router.route("/toggle/publish/:videoId").patch(togglePublishStatus);    
+
+router.route("/views/:videoId").post(increaseVideoView);    
 
 export default router
